5d9faaf407ae37005d30cce85dba57a641c8086e
Wangbang <Wangbang@ifugle.com>
2016-02-24 00:02:03 +0800

    更改页面布局

--- scripts/controllers/electronicController.js
+++ scripts/controllers/electronicController.js
@@ -30,7 +30,7 @@ angular.module('FWPT')
             $scope.setPagingFilter = function(){
                 ElectronicFileService.getList($stateParams.category).then(
                     function(data){
-                        console.log(data.result);
+                        //console.log(data.result);
                         $scope.setPagingData(data.result, $scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
                         for(var i=0;i<$scope.listData.length;i++){
                             $scope.listData[i].selected = false;
@@ -38,44 +38,52 @@ angular.module('FWPT')
                     }
                 );
             }
+            //删除事件
+            $scope.delete = ElectronicFileService.delete();
 
-            //全选
-            $scope.selectAll = function($event){
-                var checkbox = $event.target;
-                if(checkbox.checked){
-                    for(var i=0;i<$scope.listData.length;i++){
-                        $scope.listData[i].selected = true;
-                    }
-                }else{
-                    for(var i=0;i<$scope.listData.length;i++){
-                        $scope.listData[i].selected = false;
-                    }
-                }
-            }
-            //单选
-            $scope.setSelected = function($event,index){
-                var checkbox = $event.target;
-                if(checkbox.checked){
-                    $scope.listData[index].selected = true;
-                }else{
-                    $scope.listData[index].selected = false;
-                }
-            }
-            //将选好的删除条目上传服务器
-            $scope.push = function(){
-                var selected = {};
-                for(var i=0;i<$scope.listData.length;i++){
-                    if($scope.listData[i].selected = true){
-                        selected.push($scope.listData[i]);
-                    }
-                }
-                ElectronicFileService.push($scope.selected);
-                ElectronicFileService.getList($stateParams.category).then(
-                    function(data){
-                        $scope.listData = data;
-                    }
-                );
-            }
+            ////全选
+            //$scope.selectAll = function($event){
+            //    var checkbox = $event.target;
+            //    if(checkbox.checked){
+            //        for(var i=0;i<$scope.listData.length;i++){
+            //            $scope.listData[i].selected = true;
+            //        }
+            //    }else{
+            //        for(var i=0;i<$scope.listData.length;i++){
+            //            $scope.listData[i].selected = false;
+            //        }
+            //    }
+            //}
+            ////单选
+            //$scope.setSelected = function($event,index){
+            //    var checkbox = $event.target;
+            //    if(checkbox.checked){
+            //        $scope.listData[index].selected = true;
+            //    }else{
+            //        $scope.listData[index].selected = false;
+            //    }
+            //}
+            ////将选好的删除条目上传服务器
+            //$scope.push = function(){
+            //    var selected = {};
+            //    for(var i=0;i<$scope.listData.length;i++){
+            //        if($scope.listData[i].selected = true){
+            //            selected.push($scope.listData[i]);
+            //        }
+            //    }
+            //    ElectronicFileService.push($scope.selected);
+            //    ElectronicFileService.getList($stateParams.category).then(
+            //        function(data){
+            //            $scope.listData = data;
+            //        }
+            //    );
+            //}
+
+            //新增按钮发送请求
+
+    }])
+    .controller('ElectronicFileMenuController',['$scope','$state','$stateParams',
+        function($scope, $state, $stateParams){
             $scope.toggle = true;
             $scope.ulToggle = false;
             $scope.tag = function(){
@@ -87,9 +95,7 @@ angular.module('FWPT')
                     $scope.ulToggle = false;
                 }
             }
-            //新增按钮发送请求
-
-    }])
+        }])
     .controller('ElectronicFilePushController',['$scope','$state','$stateParams','ElectronicFileService',
         function ($scope, $state, $stateParams, ElectronicFileService) {
             //分页
@@ -125,42 +131,43 @@ angular.module('FWPT')
                     }
                 );
             }
-            //全选
-            $scope.selectAll = function($event){
-                var checkbox = $event.target;
-                if(checkbox.checked){
-                    for(var i=0;i<$scope.listData.length;i++){
-                        $scope.listData[i].selected = true;
-                    }
-                }else{
-                    for(var i=0;i<$scope.listData.length;i++){
-                        $scope.listData[i].selected = false;
-                    }
-                }
-            }
-            //单选
-            $scope.setSelected = function($event,index){
-                var checkbox = $event.target;
-                if(checkbox.checked){
-                    $scope.listData[index].selected = true;
-                }else{
-                    $scope.listData[index].selected = false;
-                }
-            }
-            //将选好的上报条目上传服务器
-            $scope.push = function(){
-                var selected = {};
-                for(var i=0;i<$scope.listData.length;i++){
-                    if($scope.listData[i].selected = true){
-                        selected.push($scope.listData[i]);
-                    }
-                }
-                ElectronicFileService.getList($stateParams.category).then(
-                        function(data){
-                            $scope.listData = data;
-                        }
-                );
-            }
+            $scope.push = ElectronicFileService.push();
+            ////全选
+            //$scope.selectAll = function($event){
+            //    var checkbox = $event.target;
+            //    if(checkbox.checked){
+            //        for(var i=0;i<$scope.listData.length;i++){
+            //            $scope.listData[i].selected = true;
+            //        }
+            //    }else{
+            //        for(var i=0;i<$scope.listData.length;i++){
+            //            $scope.listData[i].selected = false;
+            //        }
+            //    }
+            //}
+            ////单选
+            //$scope.setSelected = function($event,index){
+            //    var checkbox = $event.target;
+            //    if(checkbox.checked){
+            //        $scope.listData[index].selected = true;
+            //    }else{
+            //        $scope.listData[index].selected = false;
+            //    }
+            //}
+            ////将选好的上报条目上传服务器
+            //$scope.push = function(){
+            //    var selected = {};
+            //    for(var i=0;i<$scope.listData.length;i++){
+            //        if($scope.listData[i].selected = true){
+            //            selected.push($scope.listData[i]);
+            //        }
+            //    }
+            //    ElectronicFileService.getList($stateParams.category).then(
+            //            function(data){
+            //                $scope.listData = data;
+            //            }
+            //    );
+            //}
         }
     ])
     .controller('ElectronicFileModifyController',['$scope','$state','$stateParams','ElectronicFileService','$http',
